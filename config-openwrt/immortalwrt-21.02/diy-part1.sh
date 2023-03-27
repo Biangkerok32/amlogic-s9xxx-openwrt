d759fefd589fd1745ff1269a0bc731031458b184
Biangkerok32 <48493593+Biangkerok32@users.noreply.github.com>
2023-03-28 05:09:44 +0700

    Update diy-part1.sh
    Add hdragon8 package

--- config-openwrt/immortalwrt-21.02/diy-part1.sh
+++ config-openwrt/immortalwrt-21.02/diy-part1.sh
@@ -8,6 +8,7 @@
 
 # Add a feed source
 # sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+sed -i '$a src-git hdragon8 https://github.com/HDragon8/immortalwrt-newifi3-package feeds.conf.default
 
 # other
 # rm -rf package/emortal/{autosamba,ipv6-helper}
